19c55f305c16cc06587998541e31f25104e853a7
Prosper <onungwap@gmail.com>
2020-10-10 17:50:29 -0400

    test and fix valgrind

--- module3/list/Valgrind.sh
+++ module3/list/Valgrind.sh
@@ -3,16 +3,19 @@
 # A simple Shell Script that applies Valgrind to all test programs and
 # ensurses that there are no memory leaks or errors
 
+make
 for f in test_*; do
-    touch tmp.txt
-    valgrind --leak-check=full ${f} >> tmp.txt
+		valgrind --leak-check=full ${f} > tmp.txt 2>&1
     LEAK=$(grep 'no leaks are possible' tmp.txt)
     ERR=$(grep '0 errors' tmp.txt)
-    if [ ${LEAK} != 0 ] || [ ${ERR} != 0 ]; then
-	echo "memory leak or error present in file ${f}"
+		echo ${LEAK}
+		echo ${ERR}
+    if [ -z "$LEAK" ] || [ -z "$ERR" ]; then
+				echo "memory leak or error present in file ${f}"
     else
-	echo "no leaks or errors present in file ${f}"
+				echo "no leaks or errors present in file ${f}"
     fi
     rm tmp.txt
 done
+make clean
 
