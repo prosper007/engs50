132c2b6d3d9b9c2f6e296b78d58872b9997fe286
Prosper <onungwap@gmail.com>
2020-10-10 20:23:48 -0400

    start test1

--- module3/list/Valgrind.sh
+++ module3/list/Valgrind.sh
@@ -8,8 +8,6 @@ for f in test_*; do
 		valgrind --leak-check=full ${f} > tmp.txt 2>&1
     LEAK=$(grep 'no leaks are possible' tmp.txt)
     ERR=$(grep '0 errors' tmp.txt)
-		echo ${LEAK}
-		echo ${ERR}
     if [ -z "$LEAK" ] || [ -z "$ERR" ]; then
 				echo "memory leak or error present in file ${f}"
     else
