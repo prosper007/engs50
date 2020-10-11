88366afe0c4b3f64e61bd083836eb636d7f64562
Tyler Hatch <Tyler.hatch.th@dartmouth.edu>
2020-10-11 14:22:56 -0400

    changed to queue.c

--- module3/queue/queue.c
+++ module3/queue/queue.c
@@ -34,32 +34,58 @@ queue_t* find_q(queue_t *qp) {
   }
 }
 
+/* A function that creates a new queue node and allocates memory for it */
+static qnode_t* new_qnode(elementp element) {
+  qnode_t *new_node;
+  new_node = (qnode_t*)malloc(sizeof(qnode_t));
+  new_node->elementp = element;
+  new_node->next = NULL;
+  return new_node;
+}
+
 /* create an empty queue */
 queue_t* qopen(void) {
-  queue_t* queue = (queue_t*)malloc(sizeof(queue_t));
-  queue->front = queue->back = NULL;
+  queue_t* queue = (queue_t*)malloc(sizeof(queue_t)); // assign memory for queue
+  queue->front = queue->back = NULL; // set front and back to NULL
   return queue;
 }
 
 /* deallocate a queue, frees everything in it */
 void qclose(queue_t *qp) {
-  
+  while (qp->front != NULL) { // checks if qp is empty 
+    qnode_t* temp = qp->front; // store initial value of front
+    qp->front = qp->front->next; // move front to next node
+    free(temp); // free the node
+  }
+  qp->front = qp->back = NULL; // just for safety reset front and back to NULL 
+  free(qp); //free the queue
+  qp=NULL;
 }
 
+/* Function to put an element at the back of the queue */
 int32_t qput(queue_t *qp, void *elementp) {
   if (elementp == NULL) {
-    return 1;
+    return 1; // if element youre trying to add is NULL exit non-zero
   }
-  if ((front == NULL) && (back == NULL)) {
-    front = elementp;
-    back = elementp;
+  qnode_t node = new_qnode(elementp);
+  if (qp->front == NULL) {        // if queue is empty set both front and back to new node
+    qp->front = qp->back = node;
   } else {
-    back->next = elementp;
-    back = elementp;
+    qp->back->next = node;   // adding new element to a non-empty queue
+    qp->back = node;
   }
   return 0;
 }
 
+
 void* qget(queue_t *qp) {
-  front = front->next;
+  if (qp->front == NULL) {
+    return NULL;
+  }
+  qnode_t* temp = qp->front;
+  qp->front = qp->front->next;
+  if (qp->front == NULL) {
+    qp->back == NULL;
+  }
+  free(temp);
 }
